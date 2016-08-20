8e3523a6fb4811639fa346e7d844e11019090852
Kevin Boyette <kevin.boyette042@gmail.com>
2016-08-20 12:12:55 -0400

    Add .gitignore, bubble, and insertion sorts

---
+++ insertion_sort.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/python3
+
+# Insertion sort, nothing fancy and pretty standard
+
+def insertion_sort(self, array):
+    for i in range(1, len(array)):
+        x = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > x:
+            array[j + 1] = array[j]
+            j -= 1
+            array[j + 1] = x
+    return array
