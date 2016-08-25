c2b7222dd80fe198693de069e519f08f70c0f747
Kevin Boyette <kevin.boyette042@gmail.com>
2016-08-24 23:45:52 -0400

    Add docstring to Selection Sort

--- Sorts/selection_sort.py
+++ Sorts/selection_sort.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+# Kevin Boyette
+
+# TODO add cool factor to docstring
 
 
 def selection_sort(array):
+    """
+
+    Input: An array to be sorted
+    Output: A sorted array
+
+    Selection sort has a runtime of O(n**2).
+
+    :param array:
+    :return:
+    """
     for i in range(len(array) - 1):
         ith_min = i
         for j in range(i, len(array)):
