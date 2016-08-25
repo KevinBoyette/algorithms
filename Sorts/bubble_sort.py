923788b568c694ce46994c27eec19d1d26d6935f
Kevin Boyette <kevin.boyette042@gmail.com>
2016-08-24 23:35:01 -0400

    Add docstring to bubble_sort

--- Sorts/bubble_sort.py
+++ Sorts/bubble_sort.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env/python3
 
 
-# Why would you ever need this sort?
-# It's best used with the list is already sorted ;)
-
 def bubble_sort(array):
+    '''
+
+    Input: an input array to be sorted
+    Output: A sorted array
+
+    Why would you ever need this sort?
+    It's best used with the list is already sorted ;)
+
+
+    :param array:
+    :return:
+    '''
     for i in range(len(array) - 1):
         for j in range(len(array) - 1):
             if array[j] > array[j + 1]:
