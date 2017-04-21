ed6b23e9bd71084fc1cd9351076bed367d0b8ca1
Kevin Boyette <kevin.boyette042@gmail.com>
2017-04-21 08:56:45 -0400

    Add test cases for merge sort
    *  Fixed heap sort test cases (copy/paste typo)

--- Sorts/test_sorts.py
+++ Sorts/test_sorts.py
@@ -2,6 +2,7 @@ from bubble_sort import bubble_sort
 from insertion_sort import insertion_sort
 from selection_sort import selection_sort
 from heap_sort import heap_sort
+from merge_sort import merge_sort, merge
 
 class TestBubbleSort:
     def test_bubble_sort_empty(self):
@@ -41,13 +42,30 @@ class TestSelectionSort:
         assert selection_sort([2,1,2,4,3,2]) != []
         assert selection_sort([2,1,2,4,3,2]) == [1,2,2,2,3,4]
 
-class TestHeapSort():
+class TestHeapSort:
     def test_heap_sort_empty(self):
-        assert selection_sort([]) == []
+        assert heap_sort([]) == []
 
     def test_heap_sort_with_one_item(self):
-        assert selection_sort([1]) == [1]
-        assert selection_sort ([1]) != [1, 2]
+        assert heap_sort([1]) == [1]
+        assert heap_sort ([1]) != [1, 2]
     def test_heap_sort_with_many_items(self):
         assert heap_sort([2,1,2,4,3,2]) != []
         assert heap_sort([2,1,2,4,3,2]) == [1,2,2,2,3,4]
+
+class TestMergeSort:
+    def test_merge_sort_empty(self):
+        assert merge_sort([]) == []
+    
+    def test_heap_sort_with_one_item(self):
+        assert merge_sort([1]) == [1]
+        assert merge_sort ([1]) != [1, 2]
+    def test_heap_sort_with_many_items(self):
+        assert merge_sort([2,1,2,4,3,2]) != []
+        assert merge_sort([2,1,2,4,3,2]) == [1,2,2,2,3,4]
+    
+    def test_merge(self):
+        assert merge([1, 2], [2, 3]) == [1,2,2,3]
+        assert merge( [], [2, 4, 2]) == [2,4,2]
+        assert merge([], [] ) == []
+        assert merge([5,4,3], [1,7]) == [1, 5,4,3, 7]
