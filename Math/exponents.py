3f084474348fa36b7ac88adaa9ae55a2d16c4771
Kevin Boyette <kevin.boyette042@gmail.com>
2017-04-20 22:57:25 -0400

    Fix Bugs In exponents.py

--- Math/exponents.py
+++ Math/exponents.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+def isEven(n): return True if n % 2 == 0 else False
+def isOdd(n): return True if n % 2 == 1 else False
+def isNegative(n): return True if n <  0 else False
+def isZero(n): return True if n == 0 else False
+def isOne(n): return True if n == 1 else False
+
 def recur_exp_by_squaring(x, n):
     """
             Recursive method for computing x**n by
@@ -12,17 +18,11 @@ def recur_exp_by_squaring(x, n):
                      (x**2) **(n/2) if n is even
 
         """
-    if n == 0:
-        return 1
-    elif n < 0:
-        return recur_exp_by_squaring(1/x, -n)
-    elif n == 1:
-        return x
-    elif n % 2 == 0:
-        return recur_exp_by_squaring(x*x, n/2)
-    elif n % 2 == 1:
-        return x * recur_exp_by_squaring(x *x, (n - 1)/ 2)
-
+    if isZero(n): return 1
+    elif isNegative(n): return recur_exp_by_squaring(1/x, -n)
+    elif isOne(n): return x
+    elif isEven(n): return recur_exp_by_squaring(x*x, n/2)
+    elif isOdd(n): return x * recur_exp_by_squaring(x *x, (n - 1)/ 2)
 
 def iterative_squaring_exponent_method(x, n):
     """
@@ -36,19 +36,18 @@ def iterative_squaring_exponent_method(x, n):
                  (x**2) **(n/2) if n is even
 
     """
-    if n == 0:
-        return 1
-    elif n < 0:
+    print("x: {} n:{}".format(x, n))
+    if isZero(n): return 1
+    elif isNegative(n):
         x = 1/x
         n = -n
-
     y = 1
     while n > 1:
-        if n % 2 == 0:
+        if isEven(n):
             x *= x
             n /= 2
         else:
-            y = x * 2
+            y = x * 1
             x *= x
             n = (n - 1) / 2
     return x * y
