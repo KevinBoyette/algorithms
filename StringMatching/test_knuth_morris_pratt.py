b22d76842016258dc32a6d3c233d62185b53c1b8
Kevin Boyette <kevin.boyette042@gmail.com>
2017-05-31 23:07:42 -0400

    Add Knuth Morris Pratt Tests

--- StringMatching/test_knuth_morris_pratt.py
+++ StringMatching/test_knuth_morris_pratt.py
@@ -1,20 +1,30 @@
 # Test For The Knuth Morris Pratt Algorithm
-from knuth_morris_pratt import kmp, prefix_kmp
+import unittest
+from .knuth_morris_pratt import kmp, prefix_kmp
 
 
-class TestKMP:
+class TestKMP(unittest.TestCase):
     def test_kmp(self):
-        assert kmp("hello world", "ll") == 2
-        assert kmp("whomp dingo", "ding") == 6
-        assert kmp("python3 > python2", ">") == 8
-        assert kmp(" {'key' : 'value' }", ":") == 8
+        self.assertEqual(kmp("hello world", "ll"), 2)
+        self.assertEqual(kmp("whomp dingo", "ding"), 6)
+        self.assertEqual(kmp("python3 > python2", ">"), 8)
+        self.assertEqual(kmp(" {'key' : 'value' }", ":"), 8)
 
     def test_prefix_kmp(self):
-        assert prefix_kmp("world") == [-1, 0, 0, 0, 0]
-        assert prefix_kmp("tuna fish") == [-1, 0, 0, 0, 0, 0, 0, 0, 0]
-        assert prefix_kmp("?!@abcabc?!!") == [
-            -1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 2
-        ]
-        assert prefix_kmp("abcabcbacbacbabc") == [
-            -1, 0, 0, -1, 0, 0, 3, -1, 1, 0, -1, 1, 0, -1, 0, 0
-        ]
+        self.assertEqual(prefix_kmp("world"), [-1, 0, 0, 0, 0])
+        self.assertEqual(prefix_kmp("tuna fish"), [-1, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(
+            prefix_kmp("?!@abcabc?!!"), [-1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 2])
+        self.assertEqual(
+            prefix_kmp("abcabcbacbacbabc"),
+            [-1, 0, 0, -1, 0, 0, 3, -1, 1, 0, -1, 1, 0, -1, 0, 0])
+
+
+def suite():
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(TestKMP))
+    return test_suite
+
+
+runner = unittest.TextTestRunner()
+runner.run(suite())
--- Tests/runner.py
+++ Tests/runner.py
@@ -3,6 +3,7 @@ import unittest
 
 from Math import test_exponents
 from Sorts import test_sorts
+from StringMatching import test_knuth_morris_pratt
 
 # Test Suite Init
 loader = unittest.TestLoader()
@@ -11,6 +12,7 @@ suite = unittest.TestSuite()
 # Load Tests
 suite.addTests(loader.loadTestsFromModule(test_exponents))
 suite.addTests(loader.loadTestsFromModule(test_sorts))
+suite.addTests(loader.loadTestsFromModule(test_knuth_morris_pratt))
 
 # Initialize Runner
 runner = unittest.TextTestRunner(verbosity=3)
