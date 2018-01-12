5832a9fc0b45d298c626bddf46a620e4df9cbf7a
Kevin Boyette <kevin.boyette042@gmail.com>
2018-01-12 18:27:22 -0500

    Add __all__ Vars To __init__.py Files

--- sorts/__init__.py
+++ sorts/__init__.py
@@ -3,3 +3,12 @@ from .heap_sort import heap_sort
 from .insertion_sort import insertion_sort
 from .merge_sort import merge_sort
 from .selection_sort import selection_sort
+
+
+__all__ = [
+    'bubble_sort',
+    'heap_sort',
+    'insertion_sort',
+    'merge_sort',
+    'selection_sort'
+]
--- strings/__init__.py
+++ strings/__init__.py
@@ -1,1 +1,5 @@
 from .knuth_morris_pratt import kmp
+
+__all__ = [
+    'kmp',
+]
