bebf4175701426b4d1af36d78e8968c1920e8cca
Kevin Boyette <kevinxboyette@gmail.com>
2018-04-14 22:12:43 -0400

    Add Prime Factorization Algorithm

---
+++ tests/prime_factors_test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Prime Factorization Test.
+
+Author: Kevin Boyette
+"""
+from typing import Any
+
+import pytest
+
+from .context import mathematics
+
+
+@pytest.mark.parametrize("name,inputs,expected", [
+    ("positive value", 10, [2, 5]),
+    ("testing 0", 0, []),
+    ("testing 1", 1, []),
+    ("testing 2", 2, [2]),
+    ("testing 3", 3, [3]),
+    ("testing 5", 5, [5]),
+    ("testing 50", 50, [2, 5, 5]),
+    ("testing 123452", 123452, [2, 2, 7, 4409]),
+    ("testing -1", -1, []),
+])
+def test_prime_factors(name: str, inputs: int, expected: int) -> Any:
+    """Test prime factorization using the test table.
+
+    Args:
+        name (str): name of the test
+        inputs (int): testing an integer input
+        expected (int): value that is expected
+
+    """
+    assert expected == mathematics.prime_factorization(inputs), name
