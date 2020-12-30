1080fc87a43362c7f3745469db819cb34e384e3c
Kevin Boyette <kevinxboyette@gmail.com>
2020-12-30 15:10:24 -0500

    lint: make pydocstyle happy

--- algorithms/mathematics/binomial.py
+++ algorithms/mathematics/binomial.py
@@ -4,21 +4,49 @@ Author: Kevin Boyette
 """
 
 
-def factorial(n: int) -> int:
-    if n <= 1:
+def factorial(number: int) -> int:
+    """Calculate the factorial of a positive integer.
+
+    Args:
+        number (int): a positive integer
+
+    Returns:
+        int: the result of n * (n -1) * (n - 2) ... * 1
+    """
+    if number <= 1:
         return 1
-    return n * factorial(n - 1)
+    return number * factorial(number - 1)
 
 
-def n_choose_r(n: int, r: int) -> float:
-    return factorial(n) / (factorial(r) * factorial(n - r))
+def n_choose_r(total_amount: int, chosen_amount: int) -> float:
+    """Calculate C(n, r) recursively.
+
+    Args:
+        total_amount (int): total number of items in a set
+        chosen_amount (int): number of items chosen from the set
+
+    Returns:
+        int:  The result of C(n, r)
+    """
+    return factorial(total_amount) / (
+        factorial(chosen_amount) * factorial(total_amount - chosen_amount)
+    )
 
 
-def fast_binomial(n: int, r: int) -> float:
+def fast_binomial(total_amount: int, chosen_amount: int) -> float:
+    """Calculate C(n, r) iteratively.
+
+    Args:
+        total_amount (int): total number of items in a set
+        chosen_amount (int): number of items chosen from the set
+
+    Returns:
+        int:  The result of C(n, r)
+    """
     result: float = 1.0
-    if r > (n - r):
-        r = n - r
-    for i in range(0, r):
-        result *= n - i
+    if chosen_amount > (total_amount - chosen_amount):
+        chosen_amount = total_amount - chosen_amount
+    for i in range(0, chosen_amount):
+        result *= total_amount - i
         result /= i + 1
     return result
