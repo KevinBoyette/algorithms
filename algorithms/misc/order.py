5bfd97b304348b1e736a6ff3d6efc5895c7b4c2a
Kevin Boyette <kevinxboyette@gmail.com>
2022-02-10 09:00:00 -0500

    chore: simplify order further

--- algorithms/misc/order.py
+++ algorithms/misc/order.py
@@ -3,22 +3,6 @@ Author: Kevin Boyette
 """
 
 
-def _position(word: str) -> int:
-    """
-    Find where the number occurs in the word
-
-    Args:
-        word: a word that contains a number inside it
-
-    Returns:
-        int: the position in which the number occurs
-
-    """
-
-    pos = (int(number) for number in word if number.isdigit())
-    return next(pos)
-
-
 def order(sentence: str) -> str:
     """
     Reorder the sentence based on the numbers inside the words in the sentence.
@@ -30,11 +14,6 @@ def order(sentence: str) -> str:
         A sorted sentence
 
     """
-    if len(sentence) <= 1:
-        return sentence
-    split = sentence.split(" ")
-    result = [""] * (len(split) + 1)
-    for word in split:
-        result[_position(word)] = word
-
-    return " ".join(word for word in result if word != "")
+    return " ".join(
+        sorted(sentence.split(), key=lambda x: next(filter(str.isdigit, x)))
+    )
